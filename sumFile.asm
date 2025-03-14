ac3ac08c4f5fa2fc8ce35766882df8c2ab1f59fc
hunternick87 <47934810+hunternick87@users.noreply.github.com>
2025-03-13 23:34:39 -0400

    added base variables and logic to open the file

--- sumFile.asm
+++ sumFile.asm
@@ -8,16 +8,34 @@
 
 
 section .data
-    
+    file_mode db "r", 0
+    max_array_size equ 1000
+    out_format db "Sum: %d", 10, 0
+    in_format db "%d", 0
+
 section .bss
+    file_pointer resd 1                 ; pointer to file
+    number_count resd 1                 ; number of integers in the file
+    number_array resd max_array_size    ; array
+    sum resd 1                          ; sum
 
 section .text
+    global main
+    extern fopen, fscanf, printf, fclose
 
 main:
     ; prompt user or get filename from command line
-
+    mov ebx, [esp+4]
+    mov ebx, [esp+8]        ; filename argument
+    ; maybe implement error checking?
 
     ; open file
+    push mode
+    push ebx
+    call fopen
+    add esp, 8
+
+    mov [file_pointer], eax
 
 
     ; read first line of filename
