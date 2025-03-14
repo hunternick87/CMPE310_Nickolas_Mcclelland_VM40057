17ab7e5d7a768d3e00e49a8c6eb439e33d56e98e
hunternick87 <47934810+hunternick87@users.noreply.github.com>
2025-03-13 22:58:50 -0400

    layout
    started the layout of the asm file

---
+++ sumFile.asm
@@ -0,0 +1,38 @@
+;   get filename from command line
+;   open file
+;   first line of file is number of lines in file (max 1000)
+;   read the integers into an array
+;   find the sum
+;   print the sum
+;   close the file
+
+
+section .data
+    
+section .bss
+
+section .text
+
+main:
+    ; prompt user or get filename from command line
+
+
+    ; open file
+
+
+    ; read first line of filename
+
+
+    ; read integers into array
+
+
+    ; find the sum
+
+
+    ; print the sum
+
+
+    ; close the file
+
+
+    ; exit
